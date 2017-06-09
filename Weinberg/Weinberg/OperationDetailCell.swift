772db7eade67403c94ef7638929750181ff7130c
Student <student@SS17vmac03.local>
2017-06-09 12:00:42 +0200

    PrototypeCell für OperationsListe hinzugefügt

---
+++ Weinberg/Weinberg/OperationDetailCell.swift
@@ -0,0 +1,17 @@
+//
+//  OperationDetailCellTableViewCell.swift
+//  Weinberg
+//
+//  Created by Student on 09.06.17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class OperationDetailCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var done: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+}
--- Weinberg/Weinberg/OperationsViewController.swift
+++ Weinberg/Weinberg/OperationsViewController.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
-import Floaty
 
 class OperationsViewController: UIViewController {
 
     // Die Liste der Todos, die dargestellt werden soll
-    var todos:[Todo] = [Todo]()
-    
+    var ops:[Operation] = [Operation]()
+    @IBOutlet weak var operationTable: UITableView!
     
     override func viewDidLoad() {
-        todos.append(Todo(title: "BLA" , description: "BSDOIHIH"))
+        ops.append(Operation(name: "Grubbern" , date: "12.04.2017 - 15.04.2017", done: "6.84" ))
+        ops.append(Operation(name: "Schneiden" , date: "20.05.2017 - 15.06.2017", done: "0.0" ))
+        ops.append(Operation(name: "Ausbessern" , date: "11.06.2017 - 17.06.2017", done: "0.0" ))
+        ops.append(Operation(name: "Ausbrechen" , date: "12.04.2017 - 15.04.2017", done: "3.56" ))
+        ops.append(Operation(name: "Entlauben 1" , date: "12.04.2017 - 15.04.2017", done: "0.0" ))
+        ops.append(Operation(name: "Entlauben 2" , date: "12.04.2017 - 15.04.2017", done: "8.87" ))
+        ops.append(Operation(name: "Heften" , date: "12.04.2017 - 15.04.2017", done: "0.0" ))
+        ops.append(Operation(name: "Spritzen 1" , date: "12.04.2017 - 15.04.2017", done: "10.5" ))
+        ops.append(Operation(name: "Spritzen 2" , date: "12.04.2017 - 15.04.2017", done: "0.0" ))
+        ops.append(Operation(name: "Spritzen 3" , date: "12.04.2017 - 15.04.2017", done: "12.4" ))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .plain, target: nil, action: nil)
+        operationTable.tableFooterView = UIView()
     }
 
 }
@@ -25,38 +34,38 @@ class OperationsViewController: UIViewController {
 extension OperationsViewController : UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todos.count
+        return ops.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Holt ein neues Element aus der Liste aller Todos
-        let todo = todos[indexPath.row]
-        let cellIdentifier = "ElementCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        // Holt ein neues Element aus der Liste aller Operations
+        let operation = ops[indexPath.row]
+        let cellIdentifier = "OperationCell"
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? OperationDetailCell{
+            cell.name.text = operation.name
+            cell.date.text = operation.date
+            cell.done.text = operation.done
+            
+            return cell
+        }
         
-        cell.textLabel?.text = todo.title
-        cell.detailTextLabel?.text = todo.description
-        
-        return cell
+        return UITableViewCell()
     }
     
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Entfernt das ausgewählte Element aus der Liste aller Todos
-            self.todos.remove(at: indexPath.row)
-            
-            // Entfernt das Element aus der UITableView
+            // Entfernt das ausgewählte Element aus der Liste aller Operations
+            self.ops.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
 }
 
-struct Todo {
-    
-    var title: String
-    var description: String
-    
+struct Operation {
+    var name: String
+    var date: String
+    var done: String
 }
