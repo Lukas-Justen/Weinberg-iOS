ec9b461c6d39068ff5c7c196a019e48f79fb190b
Student <student@SS17vmac03.local>
2017-06-09 16:54:54 +0200

    Icons hinzugefügt

---
+++ Weinberg/Weinberg/AddOperationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddOperationViewController.swift
+//  Weinberg
+//
+//  Created by Student on 09.06.17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class AddOperationViewController: UIViewController {
+
+    @IBAction func cancleAddOperation(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
