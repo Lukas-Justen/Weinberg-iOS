1163314721126f69c675b7e00ce34863eddb6176
Student <student@SS17vmac16.local>
2017-06-14 16:45:06 +0200

    beginn Arbeit an Fields

--- Weinberg/Weinberg/FieldsViewController.swift
+++ Weinberg/Weinberg/FieldsViewController.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class FieldsViewController: UIViewController {
 
+    var fields:[Field] = [Field]()
+    
+}
+
+extension FieldsViewController : UITableViewDataSource, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fields.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
+
+struct Field {
+    var name : String
+    var fruit : String
+    var treatment : String
+    var size : Int
 }
