2f14cb9923242712726fdbb275c2112043932b6b
ema <ema@1-203-imac05.local>
2017-06-30 13:32:36 +0200

    Delegate & DataSource für OperationsViewController ausgelagert

--- Weinberg/Weinberg/OperationsViewController.swift
+++ Weinberg/Weinberg/OperationsViewController.swift
@@ -23,26 +23,26 @@ import RealmSwift
  * operations by swiping over the cell.
  */
 class OperationsViewController: UIViewController {
-
-
+    
+    
     
     // The UITableView which shows all operations
     @IBOutlet weak var operationTable: UITableView!
     // The searchBar for searching specific operations
     @IBOutlet weak var operationSearchBar: UISearchBar!
     
+    // The sort and search criteria
+    var sortOperationsBy: Int = 0
+    var sortDirection:Bool = true
+    var searchForOperations: String = ""
+    // Fills the UITableView with its data
+    var tableDelegate:OperationsVCRTableViewDelegate?
+    
     // The RealmInstance in order to access the database
     let realm = try! Realm()
     
     // The list of all operations this UIViewController displays
     var operationList: Results<Operation>?
-    // The sort and search criteria
-    var sortOperationsBy: Int = 0
-    var sortDirection:Bool = true
-    var searchForOperations: String = ""
-    // The sum of the area of all fields
-    var sumOfArea: String = ""
-
     
     
     /*
@@ -89,8 +89,11 @@ class OperationsViewController: UIViewController {
             UserDefaults.standard.set(true, forKey: "launchedBefore")
         }
         operationList = realm.objects(Operation.self).sorted(byKeyPath: "name", ascending: true)
+        tableDelegate = OperationsVCRTableViewDelegate(operationList: operationList!, navbarController: self.navigationController!, tabbarController: self.tabBarController!)
+        operationTable.delegate = tableDelegate
+        operationTable.dataSource = tableDelegate
     }
-        
+    
     /*
      * This segue closes the EditOperation- or AddOperationViewController.
      */
@@ -116,6 +119,7 @@ class OperationsViewController: UIViewController {
             operationList = operationList?.sorted(byKeyPath: "doneArea", ascending: sortDirection)
         }
         operationList = operationList?.filter("name contains '" + searchForOperations + "'")
+        tableDelegate?.operationList = operationList
         operationTable.reloadData()
     }
     
@@ -124,12 +128,6 @@ class OperationsViewController: UIViewController {
      * the UITableView updates all cells.
      */
     override func viewWillAppear(_ animated: Bool) {
-        let fields:Results<Field> = realm.objects(Field.self)
-        var area:Int64 = 0
-        for f in fields {
-            area += f.area
-        }
-        sumOfArea = String(format:"%.2f", Double(area) / 10000.0)
         updateTableView()
     }
     
@@ -138,102 +136,7 @@ class OperationsViewController: UIViewController {
 
 
 /*
- * This extension for OperationsViewController handles swipe events in order to delete
- * or edit specific operations. Furthermore it returns the number of cells going to be
- * displayed or initializes the UIViews within the OperationDetailCell. Finally it handles
- * click-events on the cells.
- */
-extension OperationsViewController : UITableViewDataSource, UITableViewDelegate {
-    
-    /*
-     * Returns the number of rows displayed by the UITableView.
-     */
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if (operationList != nil) {
-            return (operationList?.count)!
-        }
-        return realm.objects(Operation.self).count
-    }
-    
-    /*
-     * Creates an empty OperationDetailCell and fills the labels within the cell with the correct data.
-     */
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let operation = operationList![indexPath.row]
-        let cellIdentifier = "OperationDetailCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! OperationDetailCell
-        
-        cell.labelName.text = operation.name
-        cell.labelDate.text = Operation.getDateAsString(operation:operation)
-        
-        if (operation.todo.count > 0) {
-            cell.labelDone.text = String(format: "%.2f", Double(operation.doneArea) / 10000.0)
-            cell.labelAll.text = sumOfArea
-            cell.labelDone.isHidden = false
-            cell.labelAll.isHidden = false
-            cell.labelSlash.isHidden = false
-            cell.imageDone.isHidden = true
-        } else {
-            cell.labelDone.isHidden = true
-            cell.labelAll.isHidden = true
-            cell.labelSlash.isHidden = true
-            cell.imageDone.isHidden = false
-        }
-        cell.viewBackground.layer.shadowColor = UIColor.black.cgColor
-        cell.viewBackground.layer.shadowOpacity = 0.2
-        cell.viewBackground.layer.shadowOffset = CGSize.init(width: -1, height: 1)
-        cell.viewBackground.layer.shadowRadius = 1
-            
-        return cell
-    }
-    
-    /*
-     * Handles swipe-events and the editing or deleting of the selected operation.
-     */
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let editAction = UITableViewRowAction(style: .default, title: "Editieren", handler: {(action,indexPath) in
-            let o:Operation = self.operationList![indexPath.row]
-            let storyBoard: UIStoryboard = UIStoryboard(name:"Operation",bundle:nil)
-            let editController : EditOperationViewController = storyBoard.instantiateViewController(withIdentifier: "EditOperation") as! EditOperationViewController
-            editController.operation = o
-            self.navigationController?.pushViewController(editController, animated: true)
-        })
-        editAction.backgroundColor = UIColor.orange
-        
-        if (self.realm.objects(Operation.self).count > 1) {
-            let deleteAction = UITableViewRowAction(style: .default, title: "Löschen", handler: {(action,indexPath) in
-                try! self.realm.write {
-                    let operation:Operation = self.operationList![indexPath.row]
-                    if (DataManager.shared.currentOperation == operation) {
-                        DataManager.shared.currentOperation = nil
-                    }
-                    self.realm.delete(operation)
-                    DataManager.refreshOperation()
-                }
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            })
-            deleteAction.backgroundColor = UIColor.red
-            return [deleteAction, editAction]
-        }
-        
-        return [editAction]
-    }
-    
-   
-    /*
-     * Handles clicks on the OperationDetailCell
-     */
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        DataManager.shared.currentOperation = self.operationList![indexPath.row]
-        self.tabBarController?.selectedIndex = 1
-    }
-    
-}
-
-
-
-/*
- * Checks the input of the searchBar and updates the UITableView when the text within the 
+ * Checks the input of the searchBar and updates the UITableView when the text within the
  * searchbar has been changed.
  */
 extension OperationsViewController: UISearchBarDelegate {
@@ -244,3 +147,4 @@ extension OperationsViewController: UISearchBarDelegate {
     }
     
 }
+
